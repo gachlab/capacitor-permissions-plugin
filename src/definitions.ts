4252da0d95fd52cc92794c67902234150d20d93b
Guillermo <gach87@gmail.com>
2024-07-06 21:54:35 -0400

    Updated definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,7 +1,6 @@
-import { Observable } from "rxjs";
 
 export interface DevicePermissions { geolocation: 'denied' | 'granted'; notifications: 'denied' | 'granted' | 'prompt'; "notifications-policy": 'denied' | 'granted'; doNotDisturb: 0 | 1; }
 
 export interface DevicePermissionsPlugin {
-  monitor(): Observable<DevicePermissions>;
+  monitor(callback: any): void;
 }
--- src/web.ts
+++ src/web.ts
@@ -1,27 +1,24 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { DevicePermissions, DevicePermissionsPlugin } from './definitions';
-import { Observable, Subscriber } from 'rxjs';
+import type { DevicePermissionsPlugin } from './definitions';
 
 export class DevicePermissionsPluginWeb
   extends WebPlugin
   implements DevicePermissionsPlugin {
   private PermissionNames: PermissionName[] = ["geolocation", "notifications", "push"];
 
-  monitor(): Observable<DevicePermissions> {
-    return new Observable((observer: Subscriber<DevicePermissions>) => {
-      setInterval(() => {
-        Promise.all(this.PermissionNames.map((permissionName) => navigator.permissions
-          .query({ name: permissionName })
-          .then((response) => ({
-            [permissionName]: response.state,
-          }))))
-          .then((response) => response.reduce((response, permission) => {
-            response = Object.assign({}, response, permission);
-            return response;
-          }, {}))
-          .then((response: any) => observer.next(response));
-      }, 5000);
-    })
+  monitor(callback: any): void {
+    setInterval(() => {
+      Promise.all(this.PermissionNames.map((permissionName) => navigator.permissions
+        .query({ name: permissionName })
+        .then((response) => ({
+          [permissionName]: response.state,
+        }))))
+        .then((response) => response.reduce((response, permission) => {
+          response = Object.assign({}, response, permission);
+          return response;
+        }, {})).then(callback)
+    }, 5000);
+
   }
 }
